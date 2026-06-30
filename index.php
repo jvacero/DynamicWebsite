4beb81b34360497268941953c7702d582835e9c1
jvacero <johnvictorgacero@gmail.com>
2026-06-30 09:11:51 +0800

    Update

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+    include('includes/header.html')
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="includes/style.css" type="">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <div id="container">
+        <center>
+            <h1>Hello World</h1>
+        </center>
+        <center></center>
+    </div>
+        
+</body>
+</html>
+<?php include(includes/footer.html)
+?>
